266e9ed84626a758d4014a90f4f601b64ad27d62
HaykManucharyan1 <hayk.manucharyan1@tumo.org>
2024-05-14 17:17:56 +0400

    Update script.js

--- audio player3/script.js
+++ audio player3/script.js
@@ -13,7 +13,8 @@ let data = {
     "UGOVHB - WTF 2 ( PROD.EF ).mp3"],
     poster : [
     "https://i.pinimg.com/originals/3a/76/20/3a762091c6d9fb10d75ed9793d3beb29.gif",
-
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4e10ccd0-8864-444e-b827-f24cc5d28701/ddkbdmh-935df0af-5b8d-457e-b826-2db931d66bc2.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzRlMTBjY2QwLTg4NjQtNDQ0ZS1iODI3LWYyNGNjNWQyODcwMVwvZGRrYmRtaC05MzVkZjBhZi01YjhkLTQ1N2UtYjgyNi0yZGI5MzFkNjZiYzIuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.j3wHJzkjQaL1--XsihgP7kPZq_gr_eF5I-dTxPt8HtU",
+    "https://media1.giphy.com/media/T72qhZdzEpOfE5rxdC/200w.gif?cid=6c09b95256f34xtybseuucy873pqv2vze52k3jdk0g89bmae&ep=v1_gifs_search&rid=200w.gif&ct=g",
     "https://media2.giphy.com/media/bqm6WOjuLu480/200w.gif",
     "https://www.icegif.com/wp-content/uploads/icegif-2016.gif"
     ]
@@ -29,7 +30,6 @@ let data = {
 
 
     let currentSong = 0 
-    console.log(data.title[currentSong]);
 
     function playSong(){
         song.src = data.song[currentSong]
@@ -51,3 +51,47 @@ let data = {
             play.src = "images/play-button-arrowhead.png"
         }
     }
+    song.addEventListener("timeupdate",function (){
+        // console.log(song.currentTime);
+        // console.log(song.duration);
+        let fill = document.getElementsByClassName("fill")
+        let position = song.currentTime / song.duration
+        fill[0].style.marginLeft = position * 100 + "%"
+        convertTime(song.currentTime)
+        if(song.ended){
+            next()
+        }
+    })
+
+
+    function convertTime(seconds){
+        let currentTime = document.getElementsByClassName("currentTime")
+        let min = Math.floor(seconds/60)
+        let sec = Math.floor(seconds%60)
+        min = (min < 10)? "0" + min : min;
+        sec = (sec < 10)? "0" + sec : sec;
+        currentTime[0].textContent = min + ":" + sec;
+        totalTime(song.duration)
+    }
+    function totalTime(seconds){
+        let min = Math.floor(seconds / 60)
+        let sec = Math.floor(seconds % 60)
+        min = (min < 10) ? "0" + min : min;
+        sec = (sec < 10) ? "0" + sec : sec;
+        currentTime[0].textContent += "/" + "mian"
+    }
+    function next(){
+        currentSong++
+        if(currentSong >= data.song.lenght){
+            currentSong = 0 
+        }
+
+        playSong()
+    }
+  function prev(){
+      currentSong--
+      if(currentSong < 0){
+        currentSong = data.song.lenght - 1
+      }
+      playSong()
+  }
